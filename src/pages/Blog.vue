c2eacc80fb95689f939e4a3ed71bf9623de408b3
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-10-07 14:59:07 +0200

    feat: add store

---
+++ src/pages/Blog.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios'
+import {store} from '../store/store';
+
+export default {
+    name: 'Blog',
+    data(){
+        return{
+
+        }
+    },
+
+    methods:{
+        getApi(){
+            axios.get(store.apiUrl + 'posts')
+            .then(result => {
+                console.log(result.data);
+            })
+        }
+    },
+    mounted(){
+        this.getApi();
+    }
+}
+
+</script>
+
+<template>
+
+    <h1>Blog</h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus laboriosam <br> 
+        reiciendis cum totam.Porro, modi. Maxime mollitia blanditiis dolore praesentium <br>
+        ad illum! Dolore provident ducimus doloribus? Maiores fugit et adipisci.
+    </p>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../assets/pages/Home.vue'
-import About from '../assets/pages/About.vue'
-import Blog from '../assets/pages/Blog.vue'
-import Contacts from '../assets/pages/Contacts.vue'
+import Home from '../pages/Home.vue'
+import About from '../pages/About.vue'
+import Blog from '../pages/Blog.vue'
+import Contacts from '../pages/Contacts.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
